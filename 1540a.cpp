6c08271934de7ff675f3ddb37e2d93a593196b8f
Vishnu S <vishnupavan.satish@gmail.com>
2022-02-22 13:34:26 -0500

    Create 1540a.cpp

---
+++ 1540a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+// Idea: add an edge from 1 to the node with the largest d[i] value.
+// Then, add an edge from u -> v is d[v] <= d[u] with a weight of
+// d[v] - d[u]. The shortest path to go from 1 to u will go from
+// 1 -> node with largest d[i] value -> v. Every other edge will
+// remain unused in the shortest path because an edge from u -> v
+// must have an edge weight of <= d[v] - d[u], but will contribute
+// to the answer.
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> d(n + 2);
+
+    for (int i = 1; i <= n; i++) cin >> d[i];
+
+    sort(d.begin() + 1, d.begin() + n + 1);
+
+    ll ans = d[n], cs = 0;
+
+    for (int i = 1; i <= n; i++) {
+        ans -= (ll) (i - 1) * (ll) d[i] - cs;
+        cs += d[i];
+    }
+
+    cout << ans << '\n';
+}
+
+
+signed main() {
+    int t; cin >> t;
+
+    while (t--) solve();
+}
