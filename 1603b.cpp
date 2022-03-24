61a927c7532853e784df9c6038526c741f22c5da
Vishnu S <vishnupavan.satish@gmail.com>
2022-03-24 09:45:52 -0400

    Create 1603b.cpp

---
+++ 1603b.cpp
@@ -0,0 +1,33 @@
+/**
+ *    author:  vishnus
+ *    created: 2022-03-24
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Idea: for y > x, the number y - (y % x) will be divisible by x. There is a
+// point between y - (y % x) (which is the greatest number divisible by x that
+// is not greater than y) and y such that the mods will be the same. That is
+// the number between largest multiple of x less than of equal to y and y (average
+// of that value), which will be the answer.
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int x, y;
+    cin >> x >> y;
+    int ans;
+    if (x == y) {
+      cout << (ans = x) << '\n';
+    } else if (x > y) {
+      cout << (ans = x + y) << '\n';
+    } else {
+      cout << (ans = y - (y % x) / 2) << '\n';
+    }
+    assert(ans % x == y % ans);
+  }
+}
